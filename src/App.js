9ea383ffc32125a8732555fe91fda4df79271886
Jacob Chen <jacobpchen@gmail.com>
2021-01-12 21:43:44 -0500

    added initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import axios from "axios"
+class Giphy extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      trendingUrl: []
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  // on mount do an API call to get the trending gifs and load it.
+  componentDidMount(event) {
+    axios.get('https://api.giphy.com/v1/gifs/trending?api_key=rdUagZZZB6SBGDtOIGrzFOVBiDLCc94P&limit=25&rating=g')
+      .then(response => {
+        let apiResponse = response.data
+        console.log(apiResponse.data)
+        console.log(Array.isArray(apiResponse.data))
+
+        console.log(apiResponse.data[0].url)
+        this.setState({
+          trendingUrl: apiResponse.data
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        hello
+        {/* <p>{this.state.trending}</p> */}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Giphy
