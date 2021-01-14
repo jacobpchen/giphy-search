a9f3371e17aa423dd96f3a38c2c0b0dfd9e8cb6c
Abayomi Shosilva <ashosilva@gmail.com>
2021-01-14 08:59:15 -0500

    keyword gif searching

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import './App.css'
+import SearchGif from "./components/SearchGif"
 import Trending from "./components/Trending"
 
 class Giphy extends React.Component {
@@ -7,7 +8,8 @@ class Giphy extends React.Component {
     return (
       <div>
         <h2 className="header py-5 text-center">Gif Search</h2>
-        <Trending />
+        {/* <Trending /> */}
+        <SearchGif />
       </div>
     )
   }
