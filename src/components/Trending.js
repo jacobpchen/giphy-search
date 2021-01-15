b5400b8ab460f3fba34f3d45c2d2de2c2efbc0b5
zhe4ka <63822712+zhe4ka@users.noreply.github.com>
2021-01-14 20:05:01 -0500

    random

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -10,9 +10,22 @@ class Trending extends React.Component {
             data: [],
             searchWord: ' ',
             url: 'https://api.giphy.com/v1/gifs/trending?api_key=rdUagZZZB6SBGDtOIGrzFOVBiDLCc94P',
-            dataRan: ""
+            random: []
         }
     }
+    handleRandom = (event) => {
+        event.preventDefault()
+        axios.get(`http://api.giphy.com/v1/gifs/random?api_key=rdUagZZZB6SBGDtOIGrzFOVBiDLCc94P`)
+        .then(response => {
+           console.log(response);
+            this.setState({ 
+                random: [...this.state.random, response.data.data.images.original.url]
+            });
+
+        })
+        .catch(err => console.log(err));     
+    } 
+
 
     // on mount do an API call to get the trending gifs and load it.
     componentDidMount() {
@@ -27,53 +40,9 @@ class Trending extends React.Component {
                 })
             }).catch(err => console.log(err))
     }
-
-         handleRandom = (event) => {
-            event.preventDefault()
-            
-            this.setState({
-                url: `https://api.giphy.com/v1/gifs/random?api_key=rdUagZZZB6SBGDtOIGrzFOVBiDLCc94P&tag=&rating=g`,
-                dataRan: "random"
-            })
-            console.log("Printing this.state.url")
-            console.log(this.state.url)
-            console.log(typeof this.state.url)
-            //The this.setState does not update to this url when the random button is fist clicked 
-            // so I had to create a "test" variable to first get the url
-            
-            let test = "https://api.giphy.com/v1/gifs/random?api_key=rdUagZZZB6SBGDtOIGrzFOVBiDLCc94P&tag=&rating=g"
-            console.log("Printing test")
-            
-            console.log(test)
-
-            axios.get(test)
-              .then(
-                response => {
-                    let dataRes = response.data.data
-                    this.printRandomSearchGifs(dataRes)
-              })
-               .catch((err) => console.log(err)) 
-
-            // {
-            //     <GifCard
-            //         image={this.state.data.images.fixed_height.url}
-            //         //image={this.state.data.image_url}
-            //     />
-            // }
-            
-        } 
-
-        printRandomSearchGifs = (value) => { 
-            console.log("THE DATA: ")
-            console.log(value) 
-            console.log("THE GIF: " + value.images.fixed_height.url) 
-            {<GifCard
-                     image={value.images.fixed_height.url}
-                   
-                />}
-                
-          }
-        
+    
+       
+   
 
     handleSearch = (event) => {
         event.preventDefault()
@@ -100,11 +69,21 @@ class Trending extends React.Component {
         return (
             <div className="back">
                 <div className="container d-flex flex-column align-items-center justify-content-center">
-                    <button onClick={this.handleRandom}>Random</button>
+                    <button onClick={this.handleRandom}>Random</button> 
                     <form className="my-3">
                         Search For Any Gif<br />
                         <input className="mr-3" type="text" name="query" onChange={this.handleChange} />
+
                         <button onClick={this.handleSearch}>Search</button>
+                        <div>
+                        {this.state.random.map(data =>
+                                <div key={data.id} className="gif">
+                                    <GifCard
+                                        image={data} />
+                                </div>
+                        
+                         ) }
+                        </div>
                         <div className="d-flex flex-wrap">
                             {this.state.data.map(data =>
                                     <div key={data.id} className="gif">
